dd933abd612801ecc82053ed2835b91e19eb64d2
DavidMcKenzie218 <davidmckenzie218@gmail.com>
2017-01-06 16:20:03 +0000

    Displays simple message to site

---
+++ client/src/index.js
@@ -0,0 +1,11 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+window.onload = function(){
+  ReactDOM.render(
+    
+    <h1>Welcome</h1>,
+    document.getElementById('app')
+
+  );
+}
