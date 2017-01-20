81bc902de90666a8133892f1dc6ac84a899e50c0
DavidMcKenzie218 <davidmckenzie218@gmail.com>
2017-01-20 15:22:04 +0000

    Can now add a header to a content Hnadler model

--- client/src/models/content/contentHandler.js
+++ client/src/models/content/contentHandler.js
@@ -1,11 +1,19 @@
 const ContentHandler = function(){
   this.header;
   this.paragraphs = [];
+  this.images = [];
+  this.videos = [];
 }
 
 ContentHandler.prototype = {
-  all: function(){
-    console.log("ALL");
+  reset: function(){
+    delete this.header;
+    this.paragraphs = [];
+    this.images = [];
+    this.videos = [];
+  },
+  addHeader: function(newHeader){
+    this.header = newHeader;
   }
 }
 
--- client/src/models/specs/contentHandlerSpecs.js
+++ client/src/models/specs/contentHandlerSpecs.js
@@ -17,4 +17,35 @@ describe("The empty page conetent:", function(){
     assert.deepEqual([], page.paragraphs);
   })
 
+  it("should have no image links", function(){
+    assert.deepEqual([], page.images);
+  })
+
+  it("should have no video links", function(){
+    assert.deepEqual([], page.videos);
+  })
+
+})
+
+describe("A page with content:", function(){
+  
+  let page;
+  let header = "Example Header";
+
+  before(function(){
+    page = new Content();
+  })
+
+  beforeEach(function(){
+    page.reset();
+  })
+
+  it("should have a header", function(){
+    page.addHeader(header);
+    assert.deepEqual("Example Header", page.header);
+  })
+
+  it("should reset the values", function(){
+    assert.strictEqual(undefined, page.header);
+  })
 })
