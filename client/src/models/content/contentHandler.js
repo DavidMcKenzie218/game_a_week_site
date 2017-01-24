11624160b8dea0e4bbd182e341b6016e08351641
DavidMcKenzie218 <davidmckenzie218@gmail.com>
2017-01-24 15:37:48 +0000

    Model can now track the items orders in an array for eas of access

--- client/src/models/content/contentHandler.js
+++ client/src/models/content/contentHandler.js
@@ -4,6 +4,7 @@ const ContentHandler = function(){
   this.images = [];
   this.videos = [];
   this.data;
+  this.componentOrder = [];
 }
 
 ContentHandler.prototype = {
@@ -13,18 +14,40 @@ ContentHandler.prototype = {
     this.paragraphs = [];
     this.images = [];
     this.videos = [];
+    this.componentOrder = [];
   },
   addHeader: function(newHeader){
     this.header = newHeader;
+    this.componentOrder.push({component: "header"});
   },
   addParagraph: function(newParagraph){
     this.paragraphs.push(newParagraph);
+    let order = newParagraph.order;
+    if(order < this.componentOrder.length){
+      this.componentOrder.splice((order), 0,{component: "paragraph", id: this.paragraphs.length-1});
+    }else{
+      this.componentOrder.push({component: "paragraph", id: this.paragraphs.length-1})
+    }
   },
   addImage: function(newUrl){
     this.images.push(newUrl);
+    let order = newUrl.order;
+    console.log("img order: "+order)
+    console.log(this.componentOrder.length)
+    if(order < this.componentOrder.length){
+      this.componentOrder.splice((order-1), 0,{component: "image", id: this.images.length-1});
+    }else{
+      this.componentOrder.push({component: "image", id: this.images.length-1})
+    }
   },
   addVideo: function(newUrl){
     this.videos.push(newUrl);
+    let order = newUrl.order;
+    if(order < this.componentOrder.length){
+      this.componentOrder.splice((order), 0,{component: "video", id: this.videos.length-1});
+    }else{
+      this.componentOrder.push({component: "video", id: this.videos.length-1})
+    }
   },
   addData: function(data){
     this.data = data;
@@ -36,10 +59,10 @@ ContentHandler.prototype = {
     for(let x = 0; x < paraLength; x++){
       this.addParagraph(this.data.paragraphs[x]);
     }
-    this.addVideo(this.data.videos.url);
+    this.addVideo(this.data.videos);
     let imgLength = this.data.images.length;
     for(let i = 0; i < imgLength; i++){
-      this.addImage(this.data.images[i].url)
+      this.addImage(this.data.images[i])
     }
   }
 }
--- client/src/models/specs/advancedContentHandlerSpecs.js
+++ client/src/models/specs/advancedContentHandlerSpecs.js
@@ -49,12 +49,13 @@ describe("The page recieving data", function(){
 
   it("should be able to add video urls from the data", function(){
     page.addData(data);
-    assert.deepEqual(["video.url/123"], page.videos);
+    assert.deepEqual("video.url/123", page.videos[0].url);
   })
 
   it("should be able to add image urls from the data", function(){
     page.addData(data);
-    assert.deepEqual(["image.url/1", "image.url/2"], page.images);
+    assert.deepEqual("image.url/1", page.images[0].url);
+    assert.deepEqual("image.url/2", page.images[1].url);
   })
 
 })
@@ -82,9 +83,7 @@ describe("The page sorting data on positioning", function(){
     },
     images : [
       {url: "image.url/1",
-      order: 3},
-      {url: "image.url/2", 
-      order: 7}
+      order: 3}
     ]
   }  
 
@@ -108,6 +107,11 @@ describe("The page sorting data on positioning", function(){
     assert.deepEqual(2, page.paragraphs[0].order)
     assert.deepEqual(4, page.paragraphs[1].order)
     assert.deepEqual(5, page.paragraphs[2].order)
+  })
+
+  it("can sort the order of all of the components", function(){
+    page.addData(data);
+    assert.deepEqual([{component: "header"}, {component: "paragraph", id: 0}, {component: "image", id: 0}, {component: "paragraph", id: 1}, {component: "paragraph", id: 2}, {component: "video", id: 0}], page.componentOrder)
   })
 
 })
